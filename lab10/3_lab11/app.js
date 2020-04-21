67d2e7900b9d9125c5f686c4444be283a7d11cb1
crousap <timkolerov1@gmail.com>
2020-04-21 21:19:26 +0300

    Удаляю мусор

--- lab10/3_lab11/app.js
+++ lab10/3_lab11/app.js
@@ -1,8 +1,6 @@
 const joker = require('random-joke-getter')
 
 require("http").createServer((req, res) => {
-    let file = req.url.substr(1)
-    let ext = file.split('.')[1]
     console.log(`Запрашиваемый url: ${req.url}`)
     joker.getProgrammingJoke((joke) => {
         res.end(joke)
